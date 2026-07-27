f15393868126da5e3863d82c739b22cea3622b0e
juanantoniogonzalez-murciaeducaes <juanantonio.gonzalez@murciaeduca.es>
2026-07-27 18:16:07 +0200

    Fix syntax errors and clean up lambda function

--- lambda_funtion.py
+++ lambda_funtion.py
@@ -49,6 +49,6 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-       // 'headers': headers,
+       # 'headers': headers,
         'body': json.dumps({'message': 'Acceso registrado', 'ip': ip_acceso, 'timestamp': hora_acceso})
     }
